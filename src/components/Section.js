0b75b4f9a2aad31200198760178946d50d699c1f
dmitriy lazarev <web.dev.ya@ya.ru>
2023-04-04 14:47:20 +0300

    fix: remove force context binding in Section class

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,23 +3,23 @@ export default class Section {
     this._items = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
-    this.renderItem = this.renderItem.bind(this);
+    //this.renderItem = this.renderItem.bind(this);
   }
 
-  renderItems() {
+  renderItems = () => {
     this.clear();
     this._items.forEach(item => this._renderer(item));
   }
 
-  renderItem(item) {
+  renderItem = (item) => {
     this._renderer(item);
   }
 
-  clear() {
+  clear = () => {
     this._container.innerHTML = '';
   }
 
-  addItem(item) {
+  addItem = (item) => {
     this._container.prepend(item);
   }
 }
